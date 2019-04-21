91dd2c9119a63b4361530016295280494e12d0be
AleksVap <pavlo.aleks@gmail.com>
2019-04-21 17:51:06 +0200

    Creation of experiment

---
+++ preprocessData.R
@@ -0,0 +1,33 @@
+# Please set your working directory to the directory that contains this project.
+# e.g. setwd(projectDirectoryPath)
+
+
+# Load birth data from Vienna
+# Skip first row since it is the title
+# Seperator is ";" not comma!
+# second line (first not skipped line) is header:
+birthData <- read.csv2("./data/raw/vie_201.csv", header = TRUE, sep=";", skip = 1)
+
+# Data Preperation -> aggregate by reference year -> sum number of living births:
+totalBirthData <- aggregate(LIVEBIRTH ~ REF_YEAR, FUN=sum, data=birthData)
+
+
+# Load mean near surface temperature deviation
+meanTempDeviation <- read.csv2("cli_iad_td.tsv", header = TRUE, sep="\t")
+
+# Extracting the global temperature deviation
+meanTempDeviation <- meanTempDeviation[meanTempDeviation$indic_env.source.unit.time == "TD_GLB,NOAAGLOBALTEMP,DEGC", ]
+
+# Extracting global temp dev from 2002-2017
+meanTempDeviation <- meanTempDeviation[,2:(2017-2002 + 2)]
+
+# Renaming years such that they match with years from birthdate
+# Transpose globalmeantemp and convert to number
+globalTempDev <- data.frame(GlobalTempDev = as.numeric(t(meanTempDeviation)), year = c(2017:2002))
+
+
+# Merge data
+data <- merge(globalTempDev, totalBirthData, by.x = "year", by.y = "REF_YEAR")
+
+# Save preprocessed data
+write.csv2(data, "./data/processed/mergedData.csv")
